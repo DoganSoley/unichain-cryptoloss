408f54b48b81fb9d58f38d24d1d596df310d8840
Doğan Soley <110679236+DoganSoley@users.noreply.github.com>
2024-10-21 20:09:20 +0300

    script.sh

---
+++ script.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+# Renk kodları
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+NC='\033[0m'
+
+# 8. CRYPTOLOSS
+echo " "
+echo " "
+echo " "
+echo -e "${BLUE} ######  ########  ##    ## ########  ########  #######  ##        #######   ######   ######${NC}"
+echo -e "${BLUE}##    ## ##     ##  ##  ##  ##     ##    ##    ##     ## ##       ##     ## ##    ## ##    ##${NC}"
+echo -e "${BLUE}##       ##     ##   ####   ##     ##    ##    ##     ## ##       ##     ## ##       ##${NC}"
+echo -e "${BLUE}##       ########     ##    ########     ##    ##     ## ##       ##     ##  ######   ######${NC}"
+echo -e "${BLUE}##       ##   ##      ##    ##           ##    ##     ## ##       ##     ##       ##       ##${NC}"
+echo -e "${BLUE}##    ## ##    ##     ##    ##           ##    ##     ## ##       ##     ## ##    ## ##    ##${NC}"
+echo -e "${BLUE} ######  ##     ##    ##    ##           ##     #######  ########  #######   ######   ######${NC}"
+echo " "
+echo -e "${GREEN}#### Twitter : @Cryptoloss1 #####${NC}"
+echo " "
+echo " "
+
+# Sunucu güncelleme
+echo "Sunucu güncelleniyor..."
+sudo apt update && sudo apt upgrade -y
+sudo apt-get install ca-certificates curl -y
+
+# Docker yükleme
+echo "Docker yükleniyor..."
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+docker version
+
+# Docker Compose yükleme
+echo "Docker Compose yükleniyor..."
+VER=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep tag_name | cut -d '"' -f 4)
+curl -L "https://github.com/docker/compose/releases/download/$VER/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
+# Docker izinleri
+echo "Docker izinleri ayarlanıyor..."
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+# UniChain node klonlama
+echo "UniChain node klonlanıyor..."
+git clone https://github.com/Uniswap/unichain-node
+cd unichain-node
+
+# .env.sepolia parametrelerini güncelleme
+echo ".env.sepolia dosyası düzenleniyor..."
+sed -i 's|OP_NODE_L1_ETH_RPC=.*|OP_NODE_L1_ETH_RPC=https://ethereum-sepolia-rpc.publicnode.com|' .env.sepolia
+sed -i 's|OP_NODE_L1_BEACON=.*|OP_NODE_L1_BEACON=https://ethereum-sepolia-beacon-api.publicnode.com|' .env.sepolia
+
+# Node başlatma
+echo "UniChain node başlatılıyor..."
+docker-compose up -d
+echo -e "${GREEN}Ora Protokol node'u başlatılıyor 5-10dk kadar sürebilir...${NC}"
+echo " "
+echo " "
+# Node kontrolü
+echo "Node'un çalışıp çalışmadığı kontrol ediliyor..."
+curl -d '{"id":1,"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest",false]}' -H "Content-Type: application/json" http://localhost:8545
+
+echo "İşlem tamamlandı."
+
+# 8. CRYPTOLOSS
+echo " "
+echo " "
+echo " "
+echo -e "${BLUE} ######  ########  ##    ## ########  ########  #######  ##        #######   ######   ######${NC}"
+echo -e "${BLUE}##    ## ##     ##  ##  ##  ##     ##    ##    ##     ## ##       ##     ## ##    ## ##    ##${NC}"
+echo -e "${BLUE}##       ##     ##   ####   ##     ##    ##    ##     ## ##       ##     ## ##       ##${NC}"
+echo -e "${BLUE}##       ########     ##    ########     ##    ##     ## ##       ##     ##  ######   ######${NC}"
+echo -e "${BLUE}##       ##   ##      ##    ##           ##    ##     ## ##       ##     ##       ##       ##${NC}"
+echo -e "${BLUE}##    ## ##    ##     ##    ##           ##    ##     ## ##       ##     ## ##    ## ##    ##${NC}"
+echo -e "${BLUE} ######  ##     ##    ##    ##           ##     #######  ########  #######   ######   ######${NC}"
+echo " "
+echo -e "${GREEN}#### Twitter : @Cryptoloss1 #####${NC}"
+echo " "
+echo " "
+echo " "
+echo -e "${GREEN}Unichain Node kurulumu tamamlandı.${NC}"
+echo " "
+echo -e "${GREEN}Log kontrol için : ${NC}""${YELLOW}docker logs -f unichain-node-execution-client-1${NC}"
+echo " "
+echo -e "${GREEN}Sorularınız için ${NC}""${YELLOW}t.me/CryptolossChat${NC}""${GREEN} telegram kanalına gelebilirsiniz.${NC}"
+
